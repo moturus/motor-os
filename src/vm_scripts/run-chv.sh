8dcad2c77eca7c5c4479b77ace1bdc15b5d47698
U. Lasiotus <lasiotus@proton.me>
2024-02-25 12:07:23 -0800

    Update various cfg and sh files in preparation to the big NET rewrite patch. The OS won't build or run without the follow-up patch(es).

--- src/vm_scripts/run-chv.sh
+++ src/vm_scripts/run-chv.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm -f /tmp/chv
+
 cloud-hypervisor-static --cpus boot=4 --memory size=256M \
     --api-socket /tmp/chv \
     --console off --serial tty \
@@ -8,3 +10,5 @@ cloud-hypervisor-static --cpus boot=4 --memory size=256M \
     --initramfs initrd \
     --net "tap=moto-tap,mac=a4:a1:c2:00:00:01,ip=192.168.4.2,mask=255.255.255.0" \
     --disk path=moturus.full.img
+
+#           "tap=moto-tap-2,mac=a4:a1:c2:00:00:02,ip=192.168.6.2,mask=255.255.255.0" \
--- src/vm_scripts/run-qemu-full.sh
+++ src/vm_scripts/run-qemu-full.sh
@@ -5,8 +5,10 @@ qemu-system-x86_64 -m 256M -enable-kvm -cpu host -smp 4 \
   -device virtio-blk-pci,drive=drive0,id=virtblk0,num-queues=1,disable-legacy=on \
   -drive file=moturus.full.img,if=none,id=drive0,format=raw \
   -netdev tap,ifname=moto-tap,script=no,downscript=no,id=nic0 \
-  -device virtio-net-pci,disable-legacy=on,netdev=nic0 \
+  -device virtio-net-pci,disable-legacy=on,mac=a4:a1:c2:00:00:01,netdev=nic0 \
   -no-reboot -nographic
 
 #  -netdev user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10023-:5542,id=nic0 \
 
+#  -netdev tap,ifname=moto-tap-2,script=no,downscript=no,id=nic1 \
+#  -device virtio-net-pci,disable-legacy=on,mac=a4:a1:c2:00:00:02,netdev=nic1 \
