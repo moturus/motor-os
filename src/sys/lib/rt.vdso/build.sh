0bbc7e92f1d56039c58efb24c8491b616d4cb512
U. Lasiotus <lasiotus@motor-os.org>
2024-12-22 09:59:43 -0800

    rt.vdso: don't change the output file if nothing changed.
    Otherwise sys-io is always rebuilt, which takes time.

--- src/sys/lib/rt.vdso/build.sh
+++ src/sys/lib/rt.vdso/build.sh
@@ -5,6 +5,15 @@ TARGET_DIR="${CARGO_TARGET_DIR:-../../target}"
 SCRIPT_DIR="$(dirname $(readlink -f $0))"
 cd "$SCRIPT_DIR"
 
+# Don't update the output file if nothings has changed,
+# otherwise sys-io is always relinked, which takes time.
+TARGET_FNAME="${SCRIPT_DIR}/rt.vdso"
+PREV_HASH="--"
+
+if [ -f "${TARGET_FNAME}" ]; then
+    PREV_HASH=$(sha256sum "${TARGET_FNAME}" | awk '{print $1}')
+fi
+
 RUSTFLAGS="-C force-frame-pointers=yes " \
 cargo build --target rt.json -Zbuild-std=core,alloc \
   -Zbuild-std-features=compiler-builtins-mem --no-default-features $@
@@ -12,8 +21,24 @@ cargo build --target rt.json -Zbuild-std=core,alloc \
 cargo clippy --target rt.json -Zbuild-std=core,alloc \
   -Zbuild-std-features=compiler-builtins-mem --no-default-features $@
 
+# Don't update the output file if nothings has changed,
+# otherwise sys-io is always relinked, which takes time.
+TEMP_FNAME="${TARGET_FNAME}".tmp
+
 if [[ "$1" == "--release" ]] ; then
-  strip -o "${SCRIPT_DIR}/rt.vdso" "${TARGET_DIR}/rt/release/rt"
+  strip -o "${TEMP_FNAME}" "${TARGET_DIR}/rt/release/rt"
 else
-  strip -o "${SCRIPT_DIR}/rt.vdso" "${TARGET_DIR}/rt/debug/rt"
+  strip -o "${TEMP_FNAME}" "${TARGET_DIR}/rt/debug/rt"
 fi
+
+NEW_HASH=$(sha256sum "${TEMP_FNAME}" | awk '{print $1}')
+
+if [ "${NEW_HASH}" != "${PREV_HASH}" ]; then
+    rm "${TARGET_FNAME}"
+    mv "${TEMP_FNAME}" "${TARGET_FNAME}"
+    echo "did build"
+else
+    rm "${TEMP_FNAME}"
+    echo "nothing done"
+fi
+
