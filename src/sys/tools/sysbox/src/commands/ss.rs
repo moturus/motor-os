f97a41cb192da36c68427e04ee007dc86827bb6a
U. Lasiotus <lasiotus@motor-os.org>
2025-02-22 11:19:25 -0800

    sysbox/ss: print out all sockets, not just the first page.

--- src/sys/tools/sysbox/src/commands/ss.rs
+++ src/sys/tools/sysbox/src/commands/ss.rs
@@ -11,9 +11,23 @@ pub fn do_command(args: &[String]) {
     }
 
     let mut svc = moto_sys_io::stats::IoStatsService::connect().unwrap();
-    let stats = svc.get_tcp_socket_stats(0).unwrap();
 
-    for stat in stats {
-        println!("{:?}", stat);
+    let mut num_sockets = 0;
+    let mut last_socket_id = 0;
+
+    loop {
+        let stats = svc.get_tcp_socket_stats(last_socket_id).unwrap();
+        if stats.is_empty() {
+            break;
+        }
+
+        for stat in stats {
+            println!("{:?}", stat);
+        }
+
+        num_sockets += stats.len();
+        last_socket_id = stats[stats.len() - 1].id + 1;
     }
+
+    println!("\nss: {num_sockets} sockets");
 }
