b7b4b5e06e6a3e6bd04e8eca0b43a342b776c169
U. Lasiotus <lasiotus@motor-os.org>
2025-03-12 11:13:33 -0700

    moto-rt: Use Mara Bos' mutex.

--- src/sys/lib/moto-rt/src/mutex.rs
+++ src/sys/lib/moto-rt/src/mutex.rs
@@ -1,85 +1,99 @@
-// A futex-based mutex, to be used in no-std environments.
-// Inspired by spin::Mutex.
-
+/// A futex-based mutex, to be used in no-std environments.
+//
+// A slightly modified version of
+// https://github.com/m-ou-se/rust-atomics-and-locks/blob/main/src/ch9_locks/mutex_3.rs
+//
+// which has this LICENCE:
+//
+// You may use all code in this repository for any purpose.
+//
+// Attribution is appreciated, but not required.
+// An attribution usually includes the book title, author,
+// publisher, and ISBN. For example: "Rust Atomics and
+// Locks by Mara Bos (O’Reilly). Copyright 2023 Mara Bos,
+// 978-1-098-11944-7."
+//
 use core::cell::UnsafeCell;
 use core::ops::{Deref, DerefMut};
-use core::sync::atomic::{AtomicU32, Ordering};
-
-pub struct Mutex<T: ?Sized> {
-    lock: AtomicU32,
-    data: UnsafeCell<T>,
-}
-
-#[derive(Debug)]
-pub struct MutexGuard<'a, T: ?Sized> {
-    lock: &'a AtomicU32,
-    data: &'a mut T,
-}
-
-unsafe impl<T: ?Sized + Send> Sync for Mutex<T> {}
-unsafe impl<T: ?Sized + Send> Send for Mutex<T> {}
+use core::sync::atomic::AtomicU32;
+use core::sync::atomic::Ordering::{Acquire, Relaxed, Release};
 
 const UNLOCKED: u32 = 0;
-const LOCKED: u32 = 1;
+const LOCKED_NO_WAITERS: u32 = 1;
+const LOCKED_YES_WAITERS: u32 = 2;
 
-impl<T> Mutex<T> {
-    pub const fn new(user_data: T) -> Mutex<T> {
-        Mutex {
-            lock: AtomicU32::new(UNLOCKED),
-            data: UnsafeCell::new(user_data),
-        }
-    }
+pub struct Mutex<T> {
+    state: AtomicU32,
+    value: UnsafeCell<T>,
 }
 
-impl<T: ?Sized> Mutex<T> {
-    fn obtain_lock(&self) {
-        const BUSY_LOOP_ITERS: i32 = 128;
-        let mut busy_loop_counter = 0;
-        while self
-            .lock
-            .compare_exchange_weak(UNLOCKED, LOCKED, Ordering::Acquire, Ordering::Relaxed)
-            .is_err()
-        {
-            if busy_loop_counter < BUSY_LOOP_ITERS {
-                busy_loop_counter += 1;
-                core::hint::spin_loop();
-                continue;
-            }
-            crate::futex_wait(&self.lock, LOCKED, None);
-        }
-    }
+unsafe impl<T> Sync for Mutex<T> where T: Send {}
 
-    pub fn lock(&self) -> MutexGuard<'_, T> {
-        self.obtain_lock();
-        MutexGuard {
-            lock: &self.lock,
-            data: unsafe { &mut *self.data.get() },
-        }
-    }
+pub struct MutexGuard<'a, T> {
+    pub(crate) mutex: &'a Mutex<T>,
 }
 
-impl<T: Default> Default for Mutex<T> {
-    fn default() -> Mutex<T> {
-        Mutex::new(Default::default())
-    }
-}
+unsafe impl<T> Sync for MutexGuard<'_, T> where T: Sync {}
 
-impl<T: ?Sized> Deref for MutexGuard<'_, T> {
+impl<T> Deref for MutexGuard<'_, T> {
     type Target = T;
     fn deref(&self) -> &T {
-        &*self.data
+        unsafe { &*self.mutex.value.get() }
     }
 }
 
-impl<T: ?Sized> DerefMut for MutexGuard<'_, T> {
+impl<T> DerefMut for MutexGuard<'_, T> {
     fn deref_mut(&mut self) -> &mut T {
-        &mut *self.data
+        unsafe { &mut *self.mutex.value.get() }
     }
 }
 
-impl<T: ?Sized> Drop for MutexGuard<'_, T> {
+impl<T> Mutex<T> {
+    pub const fn new(value: T) -> Self {
+        Self {
+            state: AtomicU32::new(UNLOCKED),
+            value: UnsafeCell::new(value),
+        }
+    }
+
+    pub fn lock(&self) -> MutexGuard<T> {
+        if self
+            .state
+            .compare_exchange(UNLOCKED, LOCKED_NO_WAITERS, Acquire, Relaxed)
+            .is_err()
+        {
+            // The lock was already locked. :(
+            lock_contended(&self.state);
+        }
+        MutexGuard { mutex: self }
+    }
+}
+
+fn lock_contended(state: &AtomicU32) {
+    let mut spin_count = 0;
+    const MAX_BUSY_LOOP_ITERS: u32 = 100;
+
+    while state.load(Relaxed) == LOCKED_NO_WAITERS && spin_count < MAX_BUSY_LOOP_ITERS {
+        spin_count += 1;
+        core::hint::spin_loop();
+    }
+
+    if state
+        .compare_exchange(UNLOCKED, LOCKED_NO_WAITERS, Acquire, Relaxed)
+        .is_ok()
+    {
+        return;
+    }
+
+    while state.swap(LOCKED_YES_WAITERS, Acquire) != UNLOCKED {
+        crate::futex_wait(state, LOCKED_YES_WAITERS, None);
+    }
+}
+
+impl<T> Drop for MutexGuard<'_, T> {
     fn drop(&mut self) {
-        self.lock.store(UNLOCKED, Ordering::Release);
-        crate::futex_wake(self.lock);
+        if self.mutex.state.swap(UNLOCKED, Release) == LOCKED_YES_WAITERS {
+            crate::futex_wake(&self.mutex.state);
+        }
     }
 }
