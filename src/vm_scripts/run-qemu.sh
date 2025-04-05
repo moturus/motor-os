0b85d7266f2df5227e0448b037bb9701ab79c555
U. Lasiotus <lasiotus@motor-os.org>
2025-04-05 15:02:28 -0700

    Add "$@" to run-qemu.sh

--- src/vm_scripts/run-qemu.sh
+++ src/vm_scripts/run-qemu.sh
@@ -6,7 +6,7 @@ qemu-system-x86_64 -m 256M -enable-kvm -cpu host -smp 4 \
   -drive file=moturus.full.img,if=none,id=drive0,format=raw \
   -netdev tap,ifname=moto-tap,script=no,downscript=no,id=nic0 \
   -device virtio-net-pci,disable-legacy=on,mac=a4:a1:c2:00:00:01,netdev=nic0 \
-  -no-reboot -nographic
+  -no-reboot -nographic "$@"
 
 #  -netdev user,host=10.0.2.10,hostfwd=tcp:127.0.0.1:10023-:5542,id=nic0 \
 
