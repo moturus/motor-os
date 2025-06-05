8060b629f7343c15ddf7834183ba83243d45bedd
U. Lasiotus <lasiotus@motor-os.org>
2025-06-05 13:17:47 -0700

    add a full end-to-end test

---
+++ src/vm_scripts/full-test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# abort on error
+set -e
+
+# cleanup routine
+stop_vmm() {
+  ssh -p 2222 -o IdentitiesOnly=yes -i ~/tmp/motor.key motor@192.168.4.2 shutdown
+}
+
+# set the trap to call cleanup on exit
+trap stop_vmm EXIT
+
+echo "Starting Motor OS test."
+echo "Console output is redirected to /tmp/full-test.log."
+echo ""
+echo ""
+
+./run-chv.sh &> /tmp/full-test.log &
+
+sleep 1
+
+ssh -p 2222 -o IdentitiesOnly=yes -i ~/tmp/motor.key motor@192.168.4.2 sys/tests/systest
+
+ssh -p 2222 -o IdentitiesOnly=yes -i ~/tmp/motor.key motor@192.168.4.2 sys/tests/mio-test
+
+ssh -p 2222 -o IdentitiesOnly=yes -i ~/tmp/motor.key motor@192.168.4.2 sys/tests/tokio-tests
+
