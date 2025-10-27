fceadbb64d4a8954eb135432747ce7092ed715c2
U. Lasiotus <lasiotus@motor-os.org>
2025-10-26 18:17:54 -0700

    sys-io: another small step along the async runtime path (WIP)

--- src/sys/sys-io/src/runtime/fs.rs
+++ src/sys/sys-io/src/runtime/fs.rs
@@ -1,13 +1,12 @@
 use async_fs::FileSystem;
+use moto_async::{AsFuture, LocalMutex};
 use std::cell::RefCell;
 use std::io::{ErrorKind, Result};
 use std::rc::Rc;
 
 mod virtio_partition;
 
-pub(super) async fn init(
-    block_device: Rc<RefCell<virtio_async::BlockDevice>>,
-) -> Result<Box<dyn FileSystem>> {
+pub(super) async fn init(block_device: Rc<RefCell<virtio_async::BlockDevice>>) -> Result<()> {
     use zerocopy::FromZeros;
 
     let mut virtio_block = virtio_async::VirtioBlock::new_zeroed();
@@ -17,7 +16,6 @@ pub(super) async fn init(
     completion.await;
 
     let mbr = crate::fs::mbr::Mbr::parse(virtio_block.bytes.as_slice())?;
-    log::info!("Got {mbr:#?}");
 
     let mut fs: Option<Box<dyn FileSystem>> = None;
     for pte in &mbr.entries {
@@ -46,6 +44,50 @@ pub(super) async fn init(
         }
     }
 
-    fs.ok_or(ErrorKind::NotFound.into())
-        .inspect_err(|_| log::error!("Couldn't find a data partition."))
+    let Some(fs) = fs else {
+        log::error!("Couldn't find a data partition.");
+        return Err(ErrorKind::NotFound.into());
+    };
+
+    spawn_fs_listeners(fs);
+    Ok(())
+}
+
+fn spawn_fs_listeners(fs: Box<dyn FileSystem>) {
+    let fs = Rc::new(LocalMutex::new(fs));
+
+    const NUM_LISTENERS: usize = 5;
+    for _ in 0..NUM_LISTENERS {
+        spawn_new_listener(fs.clone());
+    }
+}
+
+fn spawn_new_listener(fs: Rc<LocalMutex<Box<dyn FileSystem>>>) {
+    let listener = moto_ipc::io_channel::ServerConnection::create("sys-io-fs")
+        .expect("Failed to spawn a sys-io-fs listener: {err:?}");
+
+    moto_async::LocalRuntime::spawn(async move {
+        fs_listener(fs.clone(), listener)
+            .await
+            .inspect_err(|err| log::debug!("fs_listener exited with error {err}"));
+
+        // Spawn a new listener when another one completes.
+        spawn_new_listener(fs);
+    });
+}
+
+async fn fs_listener(
+    fs: Rc<LocalMutex<Box<dyn FileSystem>>>,
+    mut listener: moto_ipc::io_channel::ServerConnection,
+) -> Result<()> {
+    listener
+        .wait_handle()
+        .as_future()
+        .await
+        .map_err(|code| std::io::Error::from_raw_os_error(code as i32))?;
+
+    listener
+        .accept()
+        .map_err(|code| std::io::Error::from_raw_os_error(code as i32))?;
+    todo!()
 }
