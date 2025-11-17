beac2b1c61516c6cfed700e7a7935e28befde2bb
aheschl1 <ajheschl@gmail.com>
2025-11-16 23:19:39 -0500

    Add argument to specify image file in run-qemu.sh
    In order to use ./run-qemu.sh to launch a specific
    image file, one needed to modify the script to use
    a different hardcoded file name. This commit adds
    an optional --img=<image_file> argument to the script.

--- src/vm_scripts/run-qemu.sh
+++ src/vm_scripts/run-qemu.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
 
 WD="$(dirname $0)"
+# the script allows for --img=<image-file> to be passed as first argument
+# if this exists, should be stripped from "$@" before passing to qemu
+IMG="motor.full.img"
+if [ "${1#--img=}" != "$1" ]; then
+  IMG="${1#--img=}"
+  shift
+fi
 
 qemu-system-x86_64 -m 256M -enable-kvm -cpu host -smp 4 \
   -device isa-debug-exit,iobase=0xf4,iosize=0x04 \
   -device virtio-blk-pci,drive=drive0,id=virtblk0,num-queues=1,disable-legacy=on \
-  -drive "file=$WD/motor.full.img,if=none,id=drive0,format=raw" \
+  -drive "file=$WD/$IMG,if=none,id=drive0,format=raw" \
   -netdev tap,ifname=moto-tap,script=no,downscript=no,id=nic0 \
   -device virtio-net-pci,disable-legacy=on,mac=a4:a1:c2:00:00:01,netdev=nic0 \
   -no-reboot -nographic "$@"
