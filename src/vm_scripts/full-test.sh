52678073754687026ed031e960131241ce2fec6e
U. Lasiotus <lasiotus@motor-os.org>
2025-12-14 14:35:20 -0800

    vm_scripts: make them runnable from any dir

--- src/vm_scripts/full-test.sh
+++ src/vm_scripts/full-test.sh
@@ -3,9 +3,11 @@
 # abort on error
 set -e
 
+WD="$(dirname $0)"
+
 # cleanup routine
 stop_vmm() {
-  ssh -p 2222 -o IdentitiesOnly=yes -i test.key motor@192.168.4.2 shutdown
+  ssh -p 2222 -o IdentitiesOnly=yes -i "$WD/test.key" motor@192.168.4.2 shutdown
 }
 
 # set the trap to call cleanup on exit
@@ -16,16 +18,17 @@ echo "Console output is redirected to /tmp/full-test.log."
 echo ""
 echo ""
 
-./run-qemu.sh &> /tmp/full-test.log &
+
+"$WD/run-qemu.sh" &> /tmp/full-test.log &
 
 # It takes some time to start sshd, especially with a debug build, so we
 # have a large timeout and several retries. And the first "test" is just an empty echo.
 
-ssh -p 2222 -o IdentitiesOnly=yes -o ConnectTimeout=30 -o ConnectionAttempts=10 -i test.key motor@192.168.4.2 /bin/echo " "
+ssh -p 2222 -o IdentitiesOnly=yes -o ConnectTimeout=30 -o ConnectionAttempts=10 -i "$WD/test.key" motor@192.168.4.2 /bin/echo " "
 
-ssh -p 2222 -o IdentitiesOnly=yes -i test.key motor@192.168.4.2 sys/tests/systest
+ssh -p 2222 -o IdentitiesOnly=yes -i "$WD/test.key" motor@192.168.4.2 sys/tests/systest
 
-ssh -p 2222 -o IdentitiesOnly=yes -i test.key motor@192.168.4.2 sys/tests/mio-test
+ssh -p 2222 -o IdentitiesOnly=yes -i "$WD/test.key" motor@192.168.4.2 sys/tests/mio-test
 
-ssh -p 2222 -o IdentitiesOnly=yes -i test.key motor@192.168.4.2 sys/tests/tokio-tests
+ssh -p 2222 -o IdentitiesOnly=yes -i "$WD/test.key" motor@192.168.4.2 sys/tests/tokio-tests
 
--- src/vm_scripts/run-chv.sh
+++ src/vm_scripts/run-chv.sh
@@ -2,13 +2,15 @@
 
 rm -f /tmp/chv
 
+WD="$(dirname $0)"
+
 cloud-hypervisor-static --cpus boot=2 --memory size=256M \
     --api-socket /tmp/chv \
     --console off --serial tty \
     --log-file /tmp/cloud-hypervisor.log -v \
-    --kernel kloader \
-    --initramfs initrd \
+    --kernel "$WD/kloader" \
+    --initramfs "$WD/initrd" \
     --net "tap=moto-tap,mac=a4:a1:c2:00:00:01,ip=192.168.4.2,mask=255.255.255.0" \
-    --disk path=motor.full.img
+    --disk path="$WD/motor.full.img"
 
 #           "tap=moto-tap-2,mac=a4:a1:c2:00:00:02,ip=192.168.6.2,mask=255.255.255.0" \
