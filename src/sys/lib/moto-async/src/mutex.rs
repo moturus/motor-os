d3d1123a559b12dad2289fb36dfe6e1b02e55ede
U. Lasiotus <lasiotus@motor-os.org>
2026-02-14 11:48:21 -0800

    moto-async: local mutex: refactor and simplify a bit

--- src/sys/lib/moto-async/src/mutex.rs
+++ src/sys/lib/moto-async/src/mutex.rs
@@ -22,8 +22,10 @@ pub struct LocalMutex<T> {
 
     // Waiters.
     next_waiter_id: RefCell<u64>,
-    waiters: RefCell<BTreeMap<u64, LocalWaker>>,
+    // Use a VecDeque to maintain mutex fairness.
     wait_queue: RefCell<VecDeque<u64>>,
+    // Use a BTreeMap to keep track of (gone) waiters and their latest wakers.
+    waiters: RefCell<BTreeMap<u64, LocalWaker>>,
 }
 
 pub struct LocalMutexGuard<'a, T> {
@@ -41,7 +43,6 @@ impl<'a, T> Drop for LocalMutexWaiter<'a, T> {
             // This waiter was polled, but then dropped without acquiring
             // the mutex (e.g. cancelled via select/timeout).
             assert!(self.mutex.waiters.borrow_mut().remove(&self.id).is_some());
-
             let Some(next_waiter_id) = self.mutex.wait_queue.borrow().front().copied() else {
                 return;
             };
@@ -61,48 +62,58 @@ impl<'a, T> Future for LocalMutexWaiter<'a, T> {
         cx: &mut core::task::Context<'_>,
     ) -> core::task::Poll<Self::Output> {
         let mut state = self.mutex.state.borrow_mut();
+        let mut wait_queue = self.mutex.wait_queue.borrow_mut();
+        let mut waiters = self.mutex.waiters.borrow_mut();
+
         if *state == State::Locked {
             if self.id == 0 {
                 self.id = self.mutex.next_waiter_id();
-                self.mutex
-                    .waiters
-                    .borrow_mut()
-                    .insert(self.id, cx.local_waker().clone());
-                self.mutex.wait_queue.borrow_mut().push_back(self.id);
+                waiters.insert(self.id, cx.local_waker().clone());
+                wait_queue.push_back(self.id);
+            } else {
+                // Update the waker.
+                *waiters.get_mut(&self.id).unwrap() = cx.local_waker().clone();
             }
+
             return core::task::Poll::Pending;
         }
 
-        self.mutex.clear_gone_waiters();
-
         // Unlocked.
-        let mut wait_queue = self.mutex.wait_queue.borrow_mut();
+        loop {
+            let Some(waiter_id) = wait_queue.front() else {
+                // No waiters.
+                debug_assert!(waiters.is_empty());
+                debug_assert_eq!(self.id, 0);
+                *state = State::Locked;
+                return core::task::Poll::Ready(LocalMutexGuard { mutex: self.mutex });
+            };
 
-        let Some(waiter_id) = wait_queue.pop_front() else {
-            // No waiters.
-            debug_assert!(self.mutex.waiters.borrow().is_empty());
-            debug_assert_eq!(self.id, 0);
-            *state = State::Locked;
-            return core::task::Poll::Ready(LocalMutexGuard { mutex: self.mutex });
-        };
+            if *waiter_id == self.id {
+                // Our turn: all is good.
+                *state = State::Locked;
+                assert!(waiters.remove(&self.id).is_some());
+                self.id = 0; // Mark not registered so that Drop behaves correctly.
+                return core::task::Poll::Ready(LocalMutexGuard { mutex: self.mutex });
+            }
 
-        if waiter_id == self.id {
-            // All is good.
-            *state = State::Locked;
-            assert!(self.mutex.waiters.borrow_mut().remove(&self.id).is_some());
-            self.id = 0; // Mark not registered so that Drop behaves correctly.
-            return core::task::Poll::Ready(LocalMutexGuard { mutex: self.mutex });
+            if let Some(waker) = waiters.get(waiter_id) {
+                // Not our turn.
+                waker.wake_by_ref();
+                if self.id == 0 {
+                    self.id = self.mutex.next_waiter_id();
+                    waiters.insert(self.id, cx.local_waker().clone());
+                    wait_queue.push_back(self.id);
+                } else {
+                    // Update the waker.
+                    *waiters.get_mut(&self.id).unwrap() = cx.local_waker().clone();
+                }
+
+                return core::task::Poll::Pending;
+            }
+
+            // The waiter is gone.
+            wait_queue.pop_front();
         }
-
-        // Not our turn.
-        wait_queue.push_front(waiter_id);
-        drop(wait_queue);
-        self.mutex
-            .waiters
-            .borrow_mut()
-            .insert(waiter_id, cx.local_waker().clone());
-        self.mutex.wake_next_waiter();
-        core::task::Poll::Pending
     }
 }
 
@@ -125,28 +136,19 @@ impl<T> LocalMutex<T> {
     }
 
     fn wake_next_waiter(&self) {
-        self.clear_gone_waiters();
-        let Some(waiter_id) = self.wait_queue.borrow().front().copied() else {
-            return;
-        };
-        self.waiters
-            .borrow()
-            .get(&waiter_id)
-            .unwrap()
-            .clone()
-            .wake()
-    }
-
-    fn clear_gone_waiters(&self) {
+        let mut wait_queue = self.wait_queue.borrow_mut();
+        let waiters = self.waiters.borrow();
         loop {
-            let Some(waiter_id) = self.wait_queue.borrow().front().copied() else {
+            let Some(waiter_id) = wait_queue.front() else {
                 return;
             };
-            if self.waiters.borrow().contains_key(&waiter_id) {
+
+            if let Some(waker) = waiters.get(waiter_id) {
+                waker.wake_by_ref();
                 return;
             }
 
-            self.wait_queue.borrow_mut().pop_front();
+            wait_queue.pop_front();
         }
     }
 
